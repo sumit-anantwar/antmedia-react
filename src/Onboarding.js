797193876a1f8dc06c409b41b2e0109da839d50e
Sumit <sumit.anantwar@gmail.com>
2022-03-20 23:02:08 +0200

    onboarding

---
+++ src/Onboarding.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import './Player.css';
+import logo from './vox_logo.svg';
+
+class Onboarding extends React.Component {
+
+    state: Object = {
+        streamName: ""
+    };
+
+    constructor(props) {
+        super(props);
+    }
+
+    componentDidMount(): void {
+        let streamId = this.props.match.params.streamId;
+        console.log(streamId)
+
+        this.props.history.push("/player/" + streamId.toUpperCase())
+    }
+
+    render() {
+
+        const { streamName } = this.state;
+
+        return (
+            <>
+                <div className="Player">
+                </div>
+            </>
+        );
+    }
+}
+
+export default JoinStream;
